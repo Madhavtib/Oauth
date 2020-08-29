76f784f0a71921c180a826eb0c902be87b3dcefe
madhav <madhavtibrewal92@gmail.com>
2020-08-29 16:32:34 +0530

    first commit

---
+++ src/test/java/com/vproject/movie/AuthorizationServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vproject.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
